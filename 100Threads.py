12c5c6f6b3b4487b7ddcbff73bc0d25db67ef477
danieldemirbag <95073147+danieldemirbag@users.noreply.github.com>
2022-11-09 14:23:32 +0100

    Add files via upload

---
+++ 100Threads.py
@@ -0,0 +1,20 @@
+import time, threading
+
+if __name__ == '__main__':
+    def task(i):
+        print(f"Task {i} starts")
+        time.sleep(1)
+        print(f"Task {i} ends")
+
+    start = time.perf_counter()
+
+    T = []
+    for i in range(100):
+        T.append(threading.Thread(target=task, args=[i]))
+    for i in range(len(T)):
+        T[i].start()
+    for i in range(len(T)):
+        T[i].join()
+
+    end = time.perf_counter()
+    print(f"Tasks ended in {round(end - start, 2)} second(s)")
