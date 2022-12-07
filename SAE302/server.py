14714516569a1701486d2d7a67320aeee4757fa4
danieldemirbag <daniel6850068@gmail.com>
2022-12-07 13:43:10 +0100

    serv + client

--- SAE302/server.py
+++ SAE302/server.py
@@ -1,2 +1,43 @@
-import platform
-print(platform.uname())
+import socket
+
+def serveur():
+    msg = ""
+    conn = None
+    server_socket = None
+    while msg != "kill" :
+        msg = ""
+        server_socket = socket.socket()
+        """ options qui permette de réutiliser l'adresse et le port rapidement"""
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# l'adresse 0.0.0.0 permet d'écouter toutes les IP de la machine, localhost, locale comme publique
+        server_socket.bind(("0.0.0.0", 15001))
+
+        server_socket.listen(1)
+        print('Serveur en attente de connexion')
+        while msg != "kill" and msg != "reset":
+            msg = ""
+            try :
+                conn, addr = server_socket.accept()
+                print (addr)
+            except ConnectionError:
+                print ("erreur de connection")
+                break
+            else :
+                while msg != "kill" and msg != "reset" and msg != "disconnect":
+                    msg = conn.recv(1024).decode()
+                    print ("Received from client: ", msg)
+                    # msg = input('Enter a message to send: ')
+                    """ 
+                    le serveur va ici récupere les commandes du client et lui renvoyer. Dans la suite de la SAÉ, 
+                    le serveur fera pareil mais en renvoyant le résultat des commandes demandées par le client.
+                    """
+                    conn.send(msg.encode())
+                conn.close()
+        print ("Connection closed")
+        server_socket.close()
+        print ("Server closed")
+
+# Coder les commande ici
+
+if __name__ == '__main__':
+    serveur()
